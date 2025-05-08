2e83e76dd088b52af030b98b089cb678c4c7ecf1
jfpa11100 <felipepala1000@gmail.com>
2025-05-08 15:18:31 -0600

    Implementing search bar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,26 +1,43 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../interfaces/product.interface';
 import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   productsService = inject(ProductService);
   products?: Product[] = [];
+  copyProducts = signal<Product[]|undefined>([]);
+  searchTerm: string = ''
 
   ngOnInit(): void {
     this.productsService.getAllProducts().subscribe({
       next: (products) => {
         this.products = products;
+        this.copyProducts?.set(this.products)
       },
       error: (err) => {
         this.products = undefined;
       },
     });
+  }
+
+  onSearch(){
+    console.log('Entre')
+    console.log(this.searchTerm)
+    if (!this.products) return
+    if (!this.searchTerm) this.copyProducts!.set(this.products);
+    else
+      this.copyProducts.set(
+        this.products.filter((product) =>
+          product.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+        )
+      );
   }
 }
